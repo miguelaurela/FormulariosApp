72b3214c7d8c9ab32eb9c65cee2fd3825b51bd97
Miguel Aurela <miguelaurela2804@gmail.com>
2022-09-09 17:20:32 -0500

    mitad de tarea de video 256

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
-
+  // miFormulario:FormGroup=new FormGroup({
+    //   nombre:     new FormControl('RTX 4080ti'),
+    //   precio:     new FormControl(1500),
+    //   existencias:new FormControl(5),
+    // })
+  miFormulario!:FormGroup
+  constructor(private _formBuilder:FormBuilder) { }
   ngOnInit(): void {
+    this.miFormulario=this._formBuilder.group({
+      nombre:[,[Validators.required,Validators.minLength(3)]],
+      precio:[,[Validators.required,Validators.min(0)]],
+      existencias:[,[Validators.required,Validators.min(0)]]
+    })
   }
-
+  campoEsValido(campo:string):boolean|null{
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+  }
+  guardar(){
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched()
+      return;
+    }
+      
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset()
+  }
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dinamicos.component.css']
 })
 export class DinamicosComponent implements OnInit {
-
-  constructor() { }
+ miFormulario!:FormGroup;
+  constructor(private _formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario=this._formBuilder.group({
+      nombre:[,[Validators.required,Validators.minLength(3)]],
+      favorito:[,[Validators.required,Validators.minLength(3)],]
+    })
+  }
+  campoEsValido(campo:string):boolean|null{
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
   }
 
+  guardar(){
+    console.log(this.miFormulario.value);
+    if (this.miFormulario.valid) {
+      this.miFormulario.markAllAsTouched()
+      return;
+    }
+  }
 }
